b76884c1af60a0ed013909a49cbd56af5c15ea0c
Carlos Pabon <carlosjavier52@gmail.com>
2025-02-09 23:16:07 -0600

    flujo de controles

---
+++ FlujosdeControl/src/SentenciaSwitchCase.java
@@ -0,0 +1,21 @@
+public class SentenciaSwitchCase {
+    public static void main(String[] args) {
+        byte num = 1;
+        switch (num){
+            case 0:
+                System.out.println("El numero es cero");
+                break;
+            case 1:
+                System.out.println("El numero es uno");
+                break;
+            case 2:
+                System.out.println("El numero es dos");
+                break;
+            case 3:
+                System.out.println("El numero es tres");
+                break;
+            default:
+                System.out.println("ese numero no procede");
+        }
+    }
+}
