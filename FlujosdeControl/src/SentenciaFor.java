0f6a430191aad900b8b21fbc1986922b9b284b56
Carlos Pabon <carlosjavier52@gmail.com>
2025-02-10 08:13:27 -0600

    agregando for while

---
+++ FlujosdeControl/src/SentenciaFor.java
@@ -0,0 +1,28 @@
+public class SentenciaFor {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 5;i++){
+            System.out.println("i = " + i);
+        }
+        System.out.println(" ======");
+
+        for(int i = 10; i >= 0; i--){
+            System.out.println("i = " + i);
+        }
+
+        System.out.println(" ======");
+
+        for(int i = 1, j = 10; i < j; i++, j--){
+            System.out.println(i + " - " + j);
+        }
+
+        System.out.println(" ======");
+        System.out.println("Imprimir numero pares");
+
+        for(int i = 0; i <= 10; i++){
+            if(!(i % 2 == 0)){
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
